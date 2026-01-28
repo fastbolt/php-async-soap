5a3eda08a369bcb64925897f6b6109fd4973eb73
DWinter <dwr@fastbolt.com>
2026-01-28 11:44:51 +0100

    Added explicit nullable types

--- src/SoapClientInterface.php
+++ src/SoapClientInterface.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Meng\AsyncSoap;
+namespace Fastbolt\AsyncSoap;
 
 interface SoapClientInterface
 {
@@ -16,30 +16,32 @@ interface SoapClientInterface
     /**
      * Synchronous SOAP call.
      *
-     * @param string $name              The name of the SOAP function to call.
-     * @param array  $arguments         An array of the arguments to pass to the function.
-     * @param array  $options           An associative array of options to pass to the client.
+     * @param string     $name          The name of the SOAP function to call.
+     * @param array      $arguments     An array of the arguments to pass to the function.
+     * @param array|null $options       An associative array of options to pass to the client.
      *                                  The location option is the URL of the remote Web service.
      *                                  The uri option is the target namespace of the SOAP service.
      *                                  The soapaction option is the action to call.
-     * @param mixed  $inputHeaders      An array of headers to be sent along with the SOAP request.
-     * @param array  $outputHeaders     If supplied, this array will be filled with the headers from the SOAP response.
+     * @param mixed      $inputHeaders  An array of headers to be sent along with the SOAP request.
+     * @param array|null $outputHeaders If supplied, this array will be filled with the headers from the SOAP response.
+     *
      * @return mixed                    The eventual value of promise returned from callAsync.
      */
-    public function call($name, array $arguments, array $options = null, $inputHeaders = null, array &$outputHeaders = null);
+    public function call($name, array $arguments, ?array $options = null, $inputHeaders = null, ?array &$outputHeaders = null);
 
     /**
      * Asynchronous SOAP call.
      *
-     * @param string $name              The name of the SOAP function to call.
-     * @param array  $arguments         An array of the arguments to pass to the function.
-     * @param array  $options           An associative array of options to pass to the client.
+     * @param string     $name          The name of the SOAP function to call.
+     * @param array      $arguments     An array of the arguments to pass to the function.
+     * @param array|null $options       An associative array of options to pass to the client.
      *                                  The location option is the URL of the remote Web service.
      *                                  The uri option is the target namespace of the SOAP service.
      *                                  The soapaction option is the action to call.
-     * @param mixed  $inputHeaders      An array of headers to be sent along with the SOAP request.
-     * @param array  $outputHeaders     If supplied, this array will be filled with the headers from the SOAP response.
+     * @param mixed      $inputHeaders  An array of headers to be sent along with the SOAP request.
+     * @param array|null $outputHeaders If supplied, this array will be filled with the headers from the SOAP response.
+     *
      * @return mixed                    A promise object whose behavior conforms to https://promisesaplus.com.
      */
-    public function callAsync($name, array $arguments, array $options = null, $inputHeaders = null, array &$outputHeaders = null);
+    public function callAsync($name, array $arguments, ?array $options = null, $inputHeaders = null, ?array &$outputHeaders = null);
 }
